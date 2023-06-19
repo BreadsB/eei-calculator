724992a6fcfb4c92d26c4a55e60eef2ad65f140c
BreadsB <102490720+BreadsB@users.noreply.github.com>
2023-06-19 21:00:15 +0200

    generatePDF method

--- src/main/java/com/breadsb/simpleapp/EEICalculatorController.java
+++ src/main/java/com/breadsb/simpleapp/EEICalculatorController.java
@@ -3,13 +3,19 @@ package com.breadsb.simpleapp;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class EEICalculatorController {
     public TextField nominalEfficiencyField;
     public TextField biomassCoefficientField;
     public TextField parameterF2Field;
     public Label eeiLabel;
     public Label energeticEfficiencyClass;
+    public Button generatePDFButton;
+    public Label productNameField;
     Calculations calculations = new Calculations();
+    private boolean isEEICalculated = false;
 
     @FXML
     protected void onCalculateButtonClick() {
@@ -18,10 +24,16 @@ public class EEICalculatorController {
             eeiLabel.setText( String.format("%.2f", result) );
             EnergeticClass eec = calculations.checkEnergeticEfficiencyClass(result);
             changeStyleOfEnergeticEfficiencyClass(eec, eec.getColorCode());
+            enablePDFButton();
         } catch (NumberFormatException | EnergeticClassException ece) {
             eeiLabel.setText("Wrong input value");
-            createAndShowAlert();
+            createAndShowAlert("Input values must be numeric and not empty!\n" +
+                    "Please fill proper values in fields");
         }
+    }
+
+    private void enablePDFButton() {
+        generatePDFButton.setDisable(false);
     }
 
     private void changeStyleOfEnergeticEfficiencyClass(EnergeticClass eec, String colour) {
@@ -30,11 +42,23 @@ public class EEICalculatorController {
         energeticEfficiencyClass.setStyle("-fx-background-color: " + colour + "; -fx-text-fill: white;");
     }
 
-    private void createAndShowAlert() {
+    private void createAndShowAlert(String text) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setHeaderText("ERROR: Please check again input values!");
+        alert.setHeaderText(text);
         alert.showAndWait();
     }
 
+    private void onGeneratePDFButtonClick() {
+        if(!productNameField.getText().isBlank()) {
+            generatePDF();
+        } else {
+            createAndShowAlert("Product name is empty. Please fill field");
+        }
+//        Generate PDF
+    }
 
+    private void generatePDF() {
+        LocalDateTime ldt = LocalDateTime.now();
+
+    }
 }
